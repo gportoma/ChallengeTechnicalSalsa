85b93c79a4e804b9a42051d2ddd71ce96919d72b
Guilherme Malta <guilherme.malta@sysmanager.com.br>
2024-03-11 09:19:00 -0300

    Refactor: Simplified makeBody method in LoginTest class

--- src/main/java/br/com/guilherme/runner/LoginTest.java
+++ src/main/java/br/com/guilherme/runner/LoginTest.java
@@ -1,14 +1,18 @@
 package br.com.guilherme.runner;
 
 import br.com.guilherme.core.RestAssuredConfig;
-import br.com.guilherme.request.Login;
+import br.com.guilherme.model.LoginModel;
 import br.com.guilherme.util.YAMLConfigReader;
+import io.restassured.http.ContentType;
 import org.junit.Before;
 import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+
 public class LoginTest {
 
-    private final Login login = new Login();
+    private final LoginModel loginModel = new LoginModel();
 
     @Before
     public void configTest() {
@@ -16,10 +20,65 @@ public class LoginTest {
     }
 
     @Test
-    public void StartTest() {
-        login.registerUser();
-        login.registerUserUnsuccessful();
-        login.loginUser();
-        login.loginUserUnsuccessful();
+    public void registerUser() {
+        makeBody("eve.holt@reqres.in", "pistol");
+        given().log().all()
+                .contentType(ContentType.JSON)
+                .body(loginModel)
+                .when()
+                .post("/register")
+                .then().log().all()
+                .statusCode(200)
+                .body("id", notNullValue())
+                .body("token", notNullValue());
+    }
+
+    @Test
+    public void loginUser() {
+        makeBody("eve.holt@reqres.in", "pistol");
+        given().log().all()
+                .contentType(ContentType.JSON)
+                .body(loginModel)
+                .when()
+                .post("/login")
+                .then().log().all()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("$", hasKey("token"));
+    }
+
+    @Test
+    public void registerUserUnsuccessful() {
+        makeBody("eve.holt@reqres.in", null);
+        given().log().all()
+                .contentType(ContentType.JSON)
+                .body(loginModel)
+                .when()
+                .post("/register")
+                .then().log().all()
+                .statusCode(400)
+                .contentType(ContentType.JSON)
+                .body("$", hasKey("error"))
+                .body("error", equalTo("Missing password"));
+    }
+
+    @Test
+    public void loginUserUnsuccessful() {
+        makeBody("eve.holt@reqres.in", null);
+        given().log().all()
+                .contentType(ContentType.JSON)
+                .body(loginModel)
+                .when()
+                .post("/login")
+                .then().log().all()
+                .statusCode(400)
+                .contentType(ContentType.JSON)
+                .body("$", hasKey("error"))
+                .body("error", equalTo("Missing password"));
+    }
+
+    private void makeBody(String email, String password) {
+        loginModel.setEmail(email);
+        loginModel.setPassword(password);
     }
 }
